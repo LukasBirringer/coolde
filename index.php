c67b0996a02beab3378e23cfa31e6ea9977308eb
Fabian Pecher <f.pecher@coopdesign.de>
2013-07-28 21:05:59 +0200

    Änderung an index.php
    Fullscreen BG-Image, Login-Form, Icons eingebunden

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <html>
 	<head>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullscreenr.js"></script>
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Raleway:400,700,200' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
 	<title>Login // coold!</title>
+	<script type="text/javascript">  
+			<!--
+				// You need to specify the size of your background image here (could be done automatically by some PHP code)
+				var FullscreenrOptions = {  width: 1920, height: 1020, bgID: '#bgimg' };
+				// This will activate the full screen background!
+				jQuery.fn.fullscreenr(FullscreenrOptions);
+			//-->
+	</script>
 	 </head>
  	<body>
-		<div class="form">
-			<h1>Login</h1>
-			<form action="login.php" method="post">
-				<input type="text" name="username" class="username" placeholder="Enter the Username">
-				<input type="text" name="password" class="password" placeholder="Enter the Username">
-				<input type="submit" value="Login">
-			</form>
+ 		<img id="bgimg" src="images/bg-login.jpg" />
+		<div class="wrapper">
+			<div class="content">
+				<h1>Login</h1>
+				<div class="form">
+					<form action="login.php" method="post">
+						<div class="inputs">
+							<i class="icon-user"></i><input type="text" name="username" class="username" placeholder="Enter the Username"><br/>
+							<i class="icon-lock"></i><input type="text" name="password" class="password" placeholder="Enter the Username">
+						</div>
+						<input type="submit" value="Login" class="login">
+					</form>
+				</div>
+			</div>				
 		</div>
 	</body>
 </html>
