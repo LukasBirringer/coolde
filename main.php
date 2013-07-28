f76dbb96ebe34e0e48bc42f533abd3700a6e1156
LukasBirringer <l.birringer@gmail.com>
2013-07-28 23:40:18 +0200

    Working DELETE/UPDATE

--- main.php
+++ main.php
@@ -1,20 +1,51 @@
 <?php 
 include "dbconfig.php";
-            mysql_connect($host,$user,$password) OR die("Unable to connect to the database");
-            mysql_select_db($db) OR die("can not select the database $db");
-            
+$sql = "SELECT id, client, todo, who, status, kategorie, added FROM coold";
+$res = mysql_query($sql);
 
- $sql = "SELECT id, client, todo, who, status, kategorie, added FROM coold";
-            $res = mysql_query($sql);
+
+function delete($id) { $result = mysql_query("DELETE FROM coold WHERE id = ".$id) or die ('Ungültige Abfrage: ' . mysql_error());
+
+if($result) { $advice = "Eintrag erfolgreich gel&ouml;scht"; } 
+else { $advice = "Fehler beim L&ouml;schen"; } 
+return $advice; }
+
+//DELETE
+if(isset($_GET['deleteid'])){
+$advice = delete($_GET['deleteid']);
+header("location:main.php");
+}
+
+//UPDATE
+if(isset($_POST['kundenname'])){
+$id = $_POST["id"]; 
+$kundenname = $_POST["kundenname"]; 
+$todo = $_POST["todo"]; 
+$who = $_POST["who"]; 
+$status = $_POST["status"];
+$result = mysql_query("UPDATE coold SET client='".$kundenname."', todo='".$todo."', who='".$who."', status='".$status."' WHERE ID = ".$id) or die (mysql_error());
+header("location:main.php");
+}
+
+//INSERT
+if(isset($_POST['kundenname'])){
+$kundenname = $_POST["kundennamenew"]; 
+$todo = $_POST["todonew"]; 
+$who = $_POST["whonew"]; 
+$status = $_POST["statusnew"];
+$result = mysql_query("INSERT INTO produkte (kundenname, todo, who, status) VALUES('".$kundenname."','".$todo."','".$who."','".$status."')") or die (mysql_error());
+header("location:main.php");
+}
 ?>
- <html>
- <head>
+<html>
+<head>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="css/main.css">
  	<title>coold!</title>
  </head>
  <body>
  	<header><a href="logout.php">Logout</a></header>
+    <div id="add">ok</div>
  	<div id="list">
  		<?php 
  		while($row = mysql_fetch_array($res)){
@@ -29,47 +60,47 @@ include "dbconfig.php";
               LÖSCHEN</p>
             <div class="editoverlay" id="over_edit_<?php echo $row['id']; ?>" href="" >
                 <p class="close">x</p>
-                <input type="text" placeholder="Kundenname">
-                <input type="text" placeholder="Beschreibung">
-                <select>
+                <form action="main.php" method="post">
+                <input type="hidden" name="id" value="<?php echo $row['id'];?>">
+                <input type="text" name="kundenname" placeholder="Kundenname">
+                <input type="text" name="todo" placeholder="Beschreibung">
+                <select name="status">
                     <option>Status</option>
-                    <option>Rot</option>
-                </select>
-                <input type="text" placeholder="Zugeteilter">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select><?php echo $row['id'];?>
+                <input type="text" name="who" placeholder="Zugeteilter">
+                <input type="submit" value="Speichern">
+                </form>
+                <a href="main.php?deleteid=<?php echo $row['id'];?>" >delete</a>
             </div>
  		</div>
  		<?php } ?>
  	</div>
  	<script type="text/javascript">
-        
-
         $(document).ready(function(){    
         	$(".button").hide();
             $(".editoverlay").hide();      
 
-            $(".element").hover(
-              function () {
-                 msg_id = $(this).attr("id");    
-                    $("#edit_" + msg_id + "").show(); 
-              },
-              function () {
-                 msg_id = $(this).attr("id");    
-                    $("#edit_" + msg_id + "").hide();
-              }
-            );
-        	
+            $(".element").hover(function () {
+                msg_id = $(this).attr("id");    
+                $("#edit_" + msg_id + "").show(); 
+            },function () {
+                msg_id = $(this).attr("id");    
+                $("#edit_" + msg_id + "").hide();
+            });   	
 
-             function hoverlink (){
-                   
-                    linkid = $(this).attr("id");    
-                    $("#over_" + linkid + "").show(); 
+            function hoverlink (){
+                linkid = $(this).attr("id");    
+                $("#over_" + linkid + "").show(); 
             }
+
             $(".button").click(hoverlink);
+
             $(".close").click(function(){
-            $(".editoverlay").hide(); 
-                
-            });
-           
+                $(".editoverlay").hide();                 
+            });           
         });
     </script>
  </body>
